b901ea1d528ff1da96eed8add74aab2191a48437
Karyna Yen <karynayen@gmail.com>
2024-01-29 21:45:01 -0500

    first working session

--- backend/internal/controller/events.go
+++ backend/internal/controller/events.go
@@ -1,1 +1,28 @@
 package controller
+
+import (
+	"context"
+	"couplet/internal/api"
+)
+
+
+func (c Controller) CreateEvent(ctx context.Context, event *api.Event) (*api.Event, error) {
+	res := c.database.Create(&event)
+
+	if res.Error != nil {
+		return nil, res.Error
+	}
+	return event, nil
+}
+
+
+func (c Controller) DeleteEventById(ctx context.Context, event api.EventId) (error) {
+	res := c.database.Delete(&api.Event{}, event)
+
+	if res.Error != nil {
+		return res.Error
+	}
+	return nil
+}
+
+
--- backend/internal/handler/events.go
+++ backend/internal/handler/events.go
@@ -1,1 +1,24 @@
 package handler
+
+import (
+	"context"
+	"couplet/internal/api"
+)
+
+func (h Handler) CreateEvent(ctx context.Context, event *api.Event) (api.CreateEventRes, error) {
+	event, err := h.controller.CreateEvent(ctx, event)
+	if err != nil {
+		return nil, err
+	}
+	return event, nil
+}
+
+
+func (h Handler) DeleteEventById(ctx context.Context, params api.DeleteEventByIdParams) (api.DeleteEventByIdRes, error) {
+	err := h.controller.DeleteEventById(ctx, params.EventId)
+	if err != nil {
+		return nil, err
+	}
+	return &api.DeleteEventByIdNoContent{}, nil
+}
+
