1ada08fab157e3101f8845574b6b48801177e166
ddusichka <78527291+ddusichka@users.noreply.github.com>
2024-01-30 22:49:15 -0500

    draft create user endpoint

--- backend/internal/controller/users.go
+++ backend/internal/controller/users.go
@@ -1,1 +1,52 @@
 package controller
+
+import (
+	"context"
+	"couplet/internal/api"
+	"fmt"
+
+	"github.com/google/uuid"
+	ht "github.com/ogen-go/ogen/http"
+	"gorm.io/gorm/clause"
+)
+
+// Creates a new user.
+// POST /users
+func (c Controller) CreateUser(ctx context.Context, req *api.CreateUserRequest) (*api.User, error) {
+	id := uuid.New();
+	fmt.Println(id)
+
+	user := api.User{
+		ID:        api.UserId(id), // Generate a new UUID
+		FirstName: req.FirstName.Value,
+		LastName:  req.LastName.Value,
+		Age:       req.Age.Value,
+	}
+	fmt.Println(user)
+
+	result := c.database.Clauses(clause.Returning{}).Create(&user)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+    return &user, nil
+}
+
+// Gets all users.
+// GET /users
+func (c Controller) GetAllUsers(ctx context.Context) ([]api.User, error) {
+	return []api.User{}, ht.ErrNotImplemented
+}
+
+// Gets a user by their user ID.
+// GET /users/{userId}
+func (c Controller) GetUserById(ctx context.Context, params api.GetUserByIdParams) (api.GetUserByIdRes, error) {
+	return &api.User{}, ht.ErrNotImplemented
+}
+
+// Deletes a user by their user ID.
+// DELETE /users/{userId}
+func (c Controller) DeleteUserById(ctx context.Context, params api.DeleteUserByIdParams) (api.DeleteUserByIdRes, error) {
+	return nil, ht.ErrNotImplemented
+}
--- backend/internal/handler/users.go
+++ backend/internal/handler/users.go
@@ -9,8 +9,9 @@ import (
 
 // Creates a new user.
 // POST /users
-func (h Handler) CreateUser(ctx context.Context, user *api.User) (api.CreateUserRes, error) {
-	return &api.User{}, ht.ErrNotImplemented
+func (h Handler) CreateUser(ctx context.Context, user *api.CreateUserRequest) (api.CreateUserRes, error) {
+	// TODO: Validate user input
+    return h.controller.CreateUser(ctx, user)
 }
 
 // Gets all users.
@@ -23,4 +24,10 @@ func (h Handler) GetAllUsers(ctx context.Context) ([]api.User, error) {
 // GET /users/{userId}
 func (h Handler) GetUserById(ctx context.Context, params api.GetUserByIdParams) (api.GetUserByIdRes, error) {
 	return &api.User{}, ht.ErrNotImplemented
+}
+
+// Deletes a user by their user ID.
+// DELETE /users/{userId}
+func (h Handler) DeleteUserById(ctx context.Context, params api.DeleteUserByIdParams) (api.DeleteUserByIdRes, error) {
+	return nil, ht.ErrNotImplemented
 }
