d3a8036171398d6b64b9f4f038cc8081dc3df568
ddusichka <78527291+ddusichka@users.noreply.github.com>
2024-02-03 15:31:36 -0500

    whitespace

--- backend/internal/controller/controller_test.go
+++ backend/internal/controller/controller_test.go
@@ -16,7 +16,7 @@ func TestNewController(t *testing.T) {
 
 	db, mock := database.NewMockDB()
 	c, err = controller.NewController(db)
-		
+
 	assert.NotEmpty(t, c)
 	assert.Nil(t, err)
 	assert.NotNil(t, mock)
@@ -30,33 +30,33 @@ func TestCreateUser(t *testing.T) {
 	// db, mock := database.NewMockDB()
 	// c, err = controller.NewController(db)
 
-	    // Create a request
-    // req, err := http.NewRequest("POST", "/users", bytes.NewBuffer(reqBytes))
-    // if err != nil {
-    //     t.Fatalf("Failed to create request: %v", err)
-    // }
+	// Create a request
+	// req, err := http.NewRequest("POST", "/users", bytes.NewBuffer(reqBytes))
+	// if err != nil {
+	//     t.Fatalf("Failed to create request: %v", err)
+	// }
 
-    // // Create a ResponseRecorder to record the response
-    // rr := httptest.NewRecorder()
+	// // Create a ResponseRecorder to record the response
+	// rr := httptest.NewRecorder()
 
-    // // Call the CreateUser handler
-    // handler := http.HandlerFunc(c.CreateUser)
-    // handler.ServeHTTP(rr, req)
+	// // Call the CreateUser handler
+	// handler := http.HandlerFunc(c.CreateUser)
+	// handler.ServeHTTP(rr, req)
 
-    // // Check the status code
-    // assert.Equal(t, http.StatusOK, rr.Code)
+	// // Check the status code
+	// assert.Equal(t, http.StatusOK, rr.Code)
 
-    // // Unmarshal the response body
-    // var resBody map[string]interface{}
-    // err = json.Unmarshal(rr.Body.Bytes(), &resBody)
-    // if err != nil {
-    //     t.Fatalf("Failed to unmarshal response body: %v", err)
-    // }
+	// // Unmarshal the response body
+	// var resBody map[string]interface{}
+	// err = json.Unmarshal(rr.Body.Bytes(), &resBody)
+	// if err != nil {
+	//     t.Fatalf("Failed to unmarshal response body: %v", err)
+	// }
 
-    // // Check the response body
-    // assert.Equal(t, "John", resBody["FirstName"])
-    // assert.Equal(t, "Doe", resBody["LastName"])
-    // assert.Equal(t, 25, resBody["Age"])
+	// // Check the response body
+	// assert.Equal(t, "John", resBody["FirstName"])
+	// assert.Equal(t, "Doe", resBody["LastName"])
+	// assert.Equal(t, 25, resBody["Age"])
 
 	// firstName, lastName, age := "John", "Doe", 25
 
@@ -70,9 +70,8 @@ func TestCreateUser(t *testing.T) {
 	// assert.Nil(t, err)
 
 	// user, err := c.CreateUser(context.Background(), "John", "Doe", 25)
-	
-	// assert.NotEmpty(t, user)
 
+	// assert.NotEmpty(t, user)
 
 	// assert.Equal(t, "John", (*user).FirstName)
 	// assert.Equal(t, "Doe", (*user).LastName)
