d73911a8c311b2900994ac514570810e258aacfd
joey-tsai <joeytsai712@gmail.com>
2024-02-03 14:51:49 -0500

    Complete Merge

--- backend/internal/controller/users.go
+++ backend/internal/controller/users.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"couplet/internal/api"
 	"couplet/internal/database"
+	"time"
 )
 
 func (c Controller) GetUserById(ctx context.Context, params api.GetUserByIdParams) (database.User, error) {
@@ -15,4 +16,48 @@ func (c Controller) GetUserById(ctx context.Context, params api.GetUserByIdParam
 	} else {
 		return user, nil
 	}
+}
+
+// func (c Controller) GetAllUsers(ctx context.Context, limit int, offset int) ([]api.User, error) {
+// 	var users []api.User
+
+// 	err := c.database.Limit(limit).Offset(offset).Find(&users).Error
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	return users, nil
+// }
+
+func (c Controller) PutUserById(ctx context.Context, userId api.UserId, updatedUser *api.User) (*api.User, error) {
+	var user api.User
+
+	// Do we create a new entry if entry doesn't exists? 
+	err := c.database.Where("id = ?", userId).First(&user).Error 
+
+	if err != nil {
+		return nil, err
+	}
+
+	userUpdates := make(map[string]interface{})
+
+	userUpdates["updatedAt"] = time.Now()
+
+	if updatedUser.FirstName != "" {
+		userUpdates["FirstName"] = updatedUser.FirstName
+	}
+
+	if updatedUser.LastName != "" {
+		userUpdates["LastName"] = updatedUser.LastName
+	}
+
+	if updatedUser.Age >= 0 {
+		userUpdates["Age"] = updatedUser.Age
+	}
+
+	if err := c.database.Model(&user).Updates(userUpdates).Error; err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
--- backend/internal/handler/users.go
+++ backend/internal/handler/users.go
@@ -7,17 +7,36 @@ import (
 	ht "github.com/ogen-go/ogen/http"
 )
 
+// GET (/users/:id) a single user by their id
+// GET (/users) all users with pagination (ask if you don't know what this is)
+// PUT (/users/:id) to completely update an existing user, returning the created object if successful
+// PATCH (/users/:id) to partially update one or many fields of an existing user, returning the created object if successful
+
 // Creates a new user.
 // POST /users
-func (h Handler) CreateUser(ctx context.Context, user *api.User) (api.CreateUserRes, error) {
-	return &api.User{}, ht.ErrNotImplemented
-}
+// func (h Handler) CreateUser(ctx context.Context, user *api.User) (api.CreateUserRes, error) {
+// 	return &api.User{}, ht.ErrNotImplemented
+// }
 
 // Gets all users.
 // GET /users
-func (h Handler) GetAllUsers(ctx context.Context) ([]api.User, error) {
-	return []api.User{}, ht.ErrNotImplemented
+// func (h Handler) GetAllUsers(ctx context.Context, params api.GetAllUsersParams) (api.GetAllUsersRes, error) {
+// 	users, err := h.controller.GetAllUsers(ctx, params.Limit, params.Offset)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return users, nil
+// }
+
+// Updates all the fields of an existing user
+func (h Handler) PutUserById(ctx context.Context, params api.PutUserByIdParams, updatedUser *api.User) (database.PutUserByIdRes, error) {
+	user, err := h.controller.PutUserById(ctx, params.UserId, updatedUser)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
 }
+
 
 // Gets a user by their user ID.
 // GET /users/{userId}
