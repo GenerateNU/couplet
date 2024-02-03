8d5ee2531838e2466b6d498149b467b3a9eb193f
Jonathan Chen <jonathan.chenjc15243@gmail.com>
2024-02-03 15:57:44 -0500

    workign post (i think)

--- backend/internal/controller/events.go
+++ backend/internal/controller/events.go
@@ -3,26 +3,36 @@ package controller
 import (
 	"context"
 	"couplet/internal/api"
+	"couplet/internal/database"
+	"time"
 )
 
-
 func (c Controller) CreateEvent(ctx context.Context, event *api.Event) (*api.Event, error) {
-	res := c.database.Create(&event)
+	newEvent := database.Event{
+		CreatedAt:      time.Now(),
+		UpdatedAt:      time.Now(),
+		Name:           event.GetName(),
+		Bio:            event.GetBio(),
+		OrganizationID: database.OrganizationID(event.OrganizationID),
+	}
 
+	res := c.database.Create(&newEvent)
 	if res.Error != nil {
 		return nil, res.Error
 	}
+
+	event.SetID(api.NewOptEventId(api.EventId(newEvent.ID)))
+	event.SetCreatedAt(api.NewOptDateTime(newEvent.CreatedAt))
+	event.SetUpdatedAt(api.NewOptDateTime(newEvent.UpdatedAt))
+
 	return event, nil
 }
 
-
-func (c Controller) DeleteEventById(ctx context.Context, event api.EventId) (error) {
-	res := c.database.Delete(&api.Event{}, event)
+func (c Controller) DeleteEventById(ctx context.Context, event api.EventId) error {
+	res := c.database.Delete(&database.Event{}, event)
 
 	if res.Error != nil {
 		return res.Error
 	}
 	return nil
 }
-
-
