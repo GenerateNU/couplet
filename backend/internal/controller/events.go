cd2daf7237036b811bec095cf56965ec97d948e5
TylerSchaefer4 <93676493+TylerSchaefer4@users.noreply.github.com>
2024-02-07 22:00:02 -0500

    backend changes without working tests

--- backend/internal/controller/events.go
+++ backend/internal/controller/events.go
@@ -1,1 +1,63 @@
 package controller
+
+import (
+	"context"
+	"couplet/internal/api"
+	"errors"
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+// GET (/events/:id) a single event by their id
+func (c *Controller) GetEventById(ctx context.Context, params api.GetEventByIdParams) (api.GetEventByIdRes, error) {
+	var event api.Event
+	result := c.database.First(&event, "id = ?", params.EventId)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return nil, fmt.Errorf("event with id %s not found", params.EventId)
+		}
+		return nil, result.Error
+	}
+
+	return &event, nil
+}
+
+// GET (/events) all events with pagination (ask if you don't know what this is)
+func (c *Controller) GetAllEvents(ctx context.Context, params api.GetAllEventsParams) (api.GetAllEventsRes, error) {
+	var events []api.Event
+	result := c.database.Limit(params.Limit.Value).Offset(params.Offset.Value).Find(&events)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return events, nil // fix this issue
+}
+
+// PUT (/events/:id) to completely update an existing event, returning the created object if successful
+func (c *Controller) UpdateEventById(ctx context.Context, req *api.Event, params api.UpdateEventByIdParams) (api.UpdateEventByIdRes, error) {
+	result := c.database.Model(&api.Event{}).Where("id = ?", params.EventId).Updates(req)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	if result.RowsAffected == 0 {
+		return nil, fmt.Errorf("event with id %s not found", params.EventId)
+	}
+
+	return req, nil
+}
+
+// PATCH (/events/:id) to partially update one or many fields of an existing event, returning the created object if successful
+func (c *Controller) PatchEventById(ctx context.Context, req *api.Event, params api.PatchEventByIdParams) (api.PatchEventByIdRes, error) {
+	result := c.database.Model(&api.Event{}).Where("id = ?", params.EventId).Updates(req)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	if result.RowsAffected == 0 {
+		return nil, fmt.Errorf("event with id %s not found", params.EventId)
+	}
+
+	return req, nil
+}
--- backend/internal/handler/events.go
+++ backend/internal/handler/events.go
@@ -1,1 +1,42 @@
 package handler
+
+import (
+	"context"
+	"couplet/internal/api"
+)
+
+func (h Handler) GetEventById(ctx context.Context, params api.GetEventByIdParams) (api.GetEventByIdRes, error) {
+	event, err := h.controller.GetEventById(ctx, params)
+	if err != nil {
+		return nil, err
+	}
+
+	return event, nil
+}
+
+func (h Handler) GetAllEvents(ctx context.Context, params api.GetAllEventsParams) (api.GetAllEventsRes, error) {
+	events, err := h.controller.GetAllEvents(ctx, params)
+	if err != nil {
+		return nil, err
+	}
+
+	return events, nil
+}
+
+func (h Handler) UpdateEventById(ctx context.Context, req *api.Event, params api.UpdateEventByIdParams) (api.UpdateEventByIdRes, error) {
+	event, err := h.controller.UpdateEventById(ctx, req, params)
+	if err != nil {
+		return nil, err
+	}
+
+	return event, nil
+}
+
+func (h Handler) PatchEventById(ctx context.Context, req *api.Event, params api.PatchEventByIdParams) (api.PatchEventByIdRes, error) {
+	event, err := h.controller.PatchEventById(ctx, req, params)
+	if err != nil {
+		return nil, err
+	}
+
+	return event, nil
+}
