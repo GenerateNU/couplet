3407de837a26f6f9868832db444180acae6007d5
Abhik Ray <abhikaboy@gmail.com>
2024-02-14 18:38:24 -0500

    new buttons

--- frontend/app/Home.tsx
+++ frontend/app/Home.tsx
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function Events() {
+export default function Home() {
   return (
     <View>
-      <Text>Events</Text>
+      <Text>Home</Text>
     </View>
   )
 }
--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,21 +1,24 @@
-import { View } from 'react-native'
-import React from 'react'
-import NavButton from './NavButton'
+import { View } from 'react-native';
+import React from 'react';
+import NavButton from './NavButton';
 
 export default function Navbar() {
-  return (
-    <View style={{
-        backgroundColor: "black",
-        height: 75,
-        width: "100%",
-        flexDirection: "row",
-        position:"absolute",
-        bottom: 0,
-      }}>
-        <NavButton route="Events"/>
-        <NavButton route="Matches" />
-        <NavButton route="Settings" />
-        <NavButton route="Users" />
-    </View>
-  )
+	return (
+		<View
+			style={{
+				backgroundColor: 'black',
+				height: 75,
+				width: '100%',
+				flexDirection: 'row',
+				position: 'absolute',
+				bottom: 0,
+			}}
+		>
+			<NavButton route="Home" />
+			<NavButton route="People" />
+			<NavButton route="Favorites" />
+			<NavButton route="Matches" />
+			<NavButton route="Profile" />
+		</View>
+	);
 }
