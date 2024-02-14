947aba9de78268ba13d14fdcadc7ec44020803db
Abhik Ray <abhikaboy@gmail.com>
2024-02-14 18:17:59 -0500

    removed unused imports

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,7 +1,6 @@
 
-import { Text, View } from "react-native"
+import { View } from "react-native"
 import HomeScreen from "../components/HomeScreen"
-import Navbar from "../components/Navbar"
 export default function index() {
   return (
     <View>
