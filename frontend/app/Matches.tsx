4aa955ddb7a349dd637a41e840b45a5931c95403
joey-tsai <joeytsai712@gmail.com>
2024-02-24 13:49:44 -0500

    Starter

--- frontend/app/Matches.tsx
+++ frontend/app/Matches.tsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { createStackNavigator } from '@react-navigation/stack';
+
+const MatchStack = createStackNavigator();
 
 export default function Matches() {
   return (
-    <View>
-      <Text>Matches</Text>
-    </View>
+    <MatchStack.Navigator>
+      <MatchStack.Screen name="Home" component={HomeScreen} />
+      <MatchStack.Screen name="Notifications" component={NotificationsScreen} />
+      <MatchStack.Screen name="Profile" component={ProfileScreen} />
+      <MatchStack.Screen name="Settings" component={SettingsScreen} />
+    </MatchStack.Navigator>
   );
 }
---
+++ frontend/components/screens/MatchHomeScreen.tsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+
+export default function MatchHomeScreen({ navigation }) {
+
+    // Code for figuring out if they have any matches 
+    const [matches, setMatches] = useState([]);
+
+    return (
+        
+    );
+};
