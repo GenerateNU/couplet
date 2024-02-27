936c2e0342265040e17664b5ed6e5bdec243c61c
TylerSchaefer4 <93676493+TylerSchaefer4@users.noreply.github.com>
2024-02-27 17:52:19 -0500

    prettier

--- frontend/app/Favorites.tsx
+++ frontend/app/Favorites.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import LikesScreen from "../components/LikesScreen";
 import { View } from "react-native";
+import LikesScreen from "../components/LikesScreen";
 import Navbar from "../components/Navbar";
 
-
 export default function Favorites() {
-
   return (
     <View style={{ flex: 1, justifyContent: "space-between" }}>
       <View style={{ flex: 1, marginBottom: 35 }}>
