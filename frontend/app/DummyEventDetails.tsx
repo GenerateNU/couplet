09a8f61b3fdf4f19893c84a87bd0867d38d14481
jtrrll <jacksonterrill3@gmail.com>
2024-02-28 16:43:10 -0500

    add types and api client to frontend

--- frontend/app/DummyEventDetails.tsx
+++ frontend/app/DummyEventDetails.tsx
@@ -1,30 +1,29 @@
 import { useRouter } from "expo-router";
 import React from "react";
-import { Button, View } from "react-native";
+import { Button, StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import EventPage from "../components/EventPage";
-import { StyleSheet } from "react-native";
 
 export default function DummyEventDetails() {
   const router = useRouter();
 
   return (
     <SafeAreaView>
-      <View style = { styles.buttonContainer}>
-      <Button title="Go back" onPress={() => router.back()} />
+      <View style={styles.buttonContainer}>
+        <Button title="Go back" onPress={() => router.back()} />
       </View>
-      <View style = { styles.cardContainer}>
-      <EventPage />
+      <View style={styles.cardContainer}>
+        <EventPage />
       </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  buttonContainer : {
+  buttonContainer: {
     transform: [{ translateY: -50 }]
   },
-  cardContainer : {
-    transform : [{ translateY : -50}]
+  cardContainer: {
+    transform: [{ translateY: -50 }]
   }
-})
+});
