59fd8c47a694bdf0e7816024d46763ecb7e8a010
Abhik Ray <abhikaboy@gmail.com>
2024-03-18 17:37:24 -0400

    photiaortiasoritjawerga

---
+++ frontend/components/PhotoPicker.tsx
@@ -0,0 +1,111 @@
+import * as ImagePicker from "expo-image-picker";
+import * as MediaLibrary from "expo-media-library";
+import React, { useState } from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { RNS3 } from "react-native-aws3";
+
+export default function PhotoPicker() {
+  const [image, setImage] = useState("");
+
+  const pick = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: false,
+      allowsMultipleSelection: true,
+      quality: 1,
+      orderedSelection: true
+    });
+    console.log(result);
+    if (!result.canceled) {
+      onDone(result.assets);
+    }
+  };
+  const openPicker = async () => {
+    const { status } = await MediaLibrary.getPermissionsAsync();
+    console.log(status);
+    if (status != "granted") {
+      // TODO: say we cant get their photos bc no permissions
+      const newPerms = await MediaLibrary.requestPermissionsAsync();
+      if (newPerms.granted) {
+        pick();
+      }
+    } else {
+      pick();
+    }
+  };
+  const onDone = (passedImages: ImagePicker.ImagePickerAsset[]) => {
+    if (typeof passedImages != "object") return;
+    if (!passedImages.hasOwnProperty("length")) return;
+
+    passedImages.forEach(async (img) => {
+      let assetInfo;
+      if (img.hasOwnProperty("assetId") && img.assetId != null)
+        assetInfo = await MediaLibrary.getAssetInfoAsync(img.assetId);
+      else return;
+      if (assetInfo.localUri == null || img.fileName == null) return;
+      const extension = assetInfo.localUri.substring(assetInfo.localUri.lastIndexOf(".") + 1);
+
+      let type = img.type + "/" + extension.toLowerCase();
+      let uri = assetInfo.localUri;
+      let name = img.fileName + new Date().getTime();
+
+      const file = {
+        uri: uri,
+        name: name,
+        type: type
+      };
+
+      const options = {
+        bucket: "relay-file-upload",
+        region: "us-east-2",
+        accessKey: process.env.EXPO_PUBLIC_AWS_ACCESS_KEY_ID || "",
+        secretKey: process.env.EXPO_PUBLIC_AWS_SECRET_ACCESS_KEY || "",
+        successActionStatus: 201
+      };
+
+      RNS3.put(file, options)
+        .then((res) => {
+          if (res.status !== 201) throw new Error("Failed to upload image to S3");
+          // We uploaded it yay! Now we can do something with the URL
+          // @ts-ignore
+          console.log(res.body.postResponse.location);
+          // @ts-ignore
+          setImage(res.body.postResponse.location);
+          // TODO: Backend call with the image we just uploaded
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    });
+  };
+  return (
+    <View>
+      <Text>PhotoPicker</Text>
+      <TouchableOpacity
+        onPress={openPicker}
+        style={{
+          width: 300,
+          height: 100,
+          backgroundColor: "white",
+          justifyContent: "center",
+          borderRadius: 10,
+          borderStyle: "solid",
+          borderWidth: 1
+        }}
+      >
+        <Text
+          style={{
+            color: "black",
+            fontSize: 24,
+            textAlign: "center"
+          }}
+        >
+          upload !!!!!!!
+        </Text>
+      </TouchableOpacity>
+
+      <Text>Image:</Text>
+      {image && <Image src={image} style={{ width: 300, height: 300 }} />}
+    </View>
+  );
+}
