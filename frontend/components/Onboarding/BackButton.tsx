dbcf18f4733130702c894cee4790e6a60c8ac4ed
ddusichka <78527291+ddusichka@users.noreply.github.com>
2024-03-22 20:02:21 -0400

    precommit checks

--- frontend/components/Onboarding/BackButton.tsx
+++ frontend/components/Onboarding/BackButton.tsx
@@ -1,40 +1,36 @@
-import React from 'react';
-import { TouchableOpacity, View, StyleSheet } from 'react-native';
-import Icon from 'react-native-vector-icons/AntDesign';
-import COLORS from '../../colors';
+import React from "react";
+import { StyleSheet, TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/AntDesign";
+import COLORS from "../../colors";
 
 type BackButtonProps = {
-  onPress: () => void; 
+  onPress: () => void;
 };
 
-function BackButton({onPress}: BackButtonProps) {
+function BackButton({ onPress }: BackButtonProps) {
   return (
     <TouchableOpacity onPress={onPress} style={styles.button}>
-      <Icon
-        name="leftcircle" 
-        size={24} 
-        color={COLORS.primary} 
-      />
+      <Icon name="leftcircle" size={24} color={COLORS.primary} />
     </TouchableOpacity>
   );
-};
+}
 
 const styles = StyleSheet.create({
   button: {
     width: 24,
     height: 24,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 100,
-    backgroundColor: COLORS.white, 
+    backgroundColor: COLORS.white,
     shadowOffset: {
       width: 0,
-      height: 2,
+      height: 2
     },
     shadowOpacity: 0.1,
     shadowRadius: 4,
-    elevation: 5,
-  },
+    elevation: 5
+  }
 });
 
 export default BackButton;
