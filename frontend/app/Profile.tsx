c01957140bd768ad7d783ea3bc15ecbcf6db3806
TylerSchaefer4 <93676493+TylerSchaefer4@users.noreply.github.com>
2024-03-25 20:48:35 -0400

    pt1 of profile components

--- frontend/app/Profile.tsx
+++ frontend/app/Profile.tsx
@@ -1,12 +1,53 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import Navbar from "../components/Layout/Navbar";
+import PurpleProfileCard from "./PurpleProfileCard";
+import UserDetails from "./UserDetails";
 
 export default function Profile() {
   return (
-    <View style={{ flex: 1, justifyContent: "space-between" }}>
-      <Text style={{ fontFamily: "DMSansRegular" }}>Profile</Text>
+    <View style={{ flex: 1 }}>
+      <Text style={{ fontFamily: "DMSansRegular" }}>ProfileA</Text>
+      <UserDetails
+        onPress={function (): void {
+          console.log("Edit Profile");
+        }}
+        profileImageSource={undefined}
+        name={"Tiffany"}
+        editDetailsText={"Edit Account Details"}
+        chevronColor={""}
+      />
+      <View style={styles.purpleCardsContainer}>
+        <PurpleProfileCard
+          imageUrl={"../assets/ProfilePurple.png"}
+          name={"My Profile"}
+          detailText={"View and make changes to your profile"}
+          onPress={function (): void {
+            console.log("My Profile");
+          }}
+        />
+        <PurpleProfileCard
+          imageUrl={"../assets/ProfilePurple.png"}
+          name={"My Preferences"}
+          detailText={"Manage what you are looking for"}
+          onPress={function (): void {
+            console.log("My Preferences");
+          }}
+        />
+      </View>
+
       <Navbar />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  purpleCardsContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "flex-start",
+    gap: 20,
+    alignSelf: "stretch",
+    flexDirection: "row"
+  }
+});
---
+++ frontend/app/UserDetails.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+interface UserDetailsProps {
+  onPress: () => void;
+  profileImageSource: any; // Adjust the type as needed
+  name: string;
+  editDetailsText: string;
+  chevronColor: string;
+}
+
+export default function UserDetails(props: UserDetailsProps) {
+  const { onPress, profileImageSource, name, editDetailsText, chevronColor } = props;
+
+  return (
+    <TouchableOpacity onPress={onPress} style={styles.container}>
+      <View style={styles.profileNameContainer}>
+        <Image source={require("../assets/stockProPic.png")} style={styles.imageContainer} />
+        <View style={styles.nameAndEditContainer}>
+          <Text style={styles.name}>{name}</Text>
+          <Text style={styles.edit}>{editDetailsText}</Text>
+        </View>
+      </View>
+      <View style={[styles.chevronContainer, { backgroundColor: chevronColor }]}>
+        <Text style={styles.chevron}>&gt;</Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    width: 349,
+    padding: 16,
+    justifyContent: "space-between",
+    alignContent: "center",
+    elevation: 5,
+    borderRadius: 8,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    backgroundColor: "#FFF",
+    flexDirection: "row"
+  },
+  profileNameContainer: {
+    width: 164,
+    height: 54,
+    flexShrink: 0,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 16
+  },
+  imageContainer: {
+    width: 54,
+    height: 54,
+    borderRadius: 27,
+    backgroundColor: "#0F0"
+  },
+  nameAndEditContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start"
+  },
+  name: {
+    fontSize: 16,
+    fontFamily: "DMSansRegular",
+    color: "#222",
+    fontStyle: "normal",
+    fontWeight: "700",
+    lineHeight: 21
+  },
+  edit: {
+    fontSize: 10,
+    fontFamily: "DMSansRegular",
+    color: "#222",
+    fontStyle: "normal",
+    fontWeight: "400",
+    lineHeight: 13
+  },
+  chevronContainer: {
+    height: "auto",
+    backgroundColor: "#0FF",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  chevron: {
+    fontSize: 16,
+    color: "#222",
+    lineHeight: 21
+  }
+});
