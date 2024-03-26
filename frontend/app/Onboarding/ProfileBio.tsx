24236d4a2d8c70ffa6adfdab8ef3e85c0729e8f3
Jonathan Chen <jonathan.chenjc15243@gmail.com>
2024-03-26 17:13:07 -0400

    basic prompt dropdown

--- frontend/app/Onboarding/ProfileBio.tsx
+++ frontend/app/Onboarding/ProfileBio.tsx
@@ -1,6 +1,7 @@
 import { router } from "expo-router";
 import React, { useRef, useState } from "react";
 import { Image, Keyboard, StyleSheet, TextInput, View } from "react-native";
+import DropDownPicker from "react-native-dropdown-picker";
 import ContinueButton from "../../components/Onboarding/ContinueButton";
 import OnboardingTitle from "../../components/Onboarding/OnboardingTitle";
 import TopBar from "../../components/Onboarding/TopBar";
@@ -8,8 +9,17 @@ import TopBar from "../../components/Onboarding/TopBar";
 const BIO_IMAGE = require("../../assets/profilebio.png");
 
 export default function ProfileBio() {
-  const [bio, setBio] = useState<string>("");
-  const [inputStyle, setInputStyle] = useState(styles.unfocusedInput);
+  const [open, setOpen] = useState<boolean>(false);
+  const [prompt, setPrompt] = useState(null);
+  const [options, setOptions] = useState([
+    { label: "An ideal date", value: 0 },
+    { label: "My perfect day consists of", value: 1 },
+    { label: "On weekends you can find me", value: 2 },
+    { label: "What I'm looking for on this app", value: 3 },
+    { label: "One thing you should know about me", value: 4 }
+  ]);
+  const [response, setResponse] = useState<string>("");
+  const [inputStyle, setInputStyle] = useState(styles.unfocusedResponse);
   const inputRef = useRef(null);
 
   const onContinue = () => {
@@ -25,20 +35,34 @@ export default function ProfileBio() {
       <View>
         <Image source={BIO_IMAGE} />
         <OnboardingTitle text="What's your bio?" />
+        <DropDownPicker
+          style={prompt !== null ? styles.chosenPrompt : styles.unchosenPrompt}
+          placeholder="Select a prompt"
+          open={open}
+          value={prompt}
+          items={options}
+          setOpen={setOpen}
+          setValue={setPrompt}
+          setItems={setOptions}
+        />
         <TextInput
           ref={inputRef}
-          onFocus={() => setInputStyle(styles.focusedInput)}
-          onBlur={() => setInputStyle(styles.unfocusedInput)}
+          onFocus={() => setInputStyle(styles.focusedResponse)}
+          onBlur={() => setInputStyle(styles.unfocusedResponse)}
           onSubmitEditing={() => Keyboard.dismiss()}
           style={inputStyle}
-          onChangeText={setBio}
-          value={bio}
-          placeholder="Your bio here (optional)"
+          onChangeText={setResponse}
+          value={response}
+          placeholder="Your response here"
         />
       </View>
       <View />
       <View>
-        <ContinueButton title="Continue" isDisabled={false} onPress={onContinue} />
+        <ContinueButton
+          title="Continue"
+          isDisabled={!(prompt !== null && response !== "")}
+          onPress={onContinue}
+        />
       </View>
     </View>
   );
@@ -51,14 +75,20 @@ const styles = StyleSheet.create({
     alignItems: "center",
     margin: 30
   },
-  focusedInput: {
-    padding: 5,
+  chosenPrompt: {
+    marginBottom: 10
+  },
+  unchosenPrompt: {
+    marginBottom: 10
+  },
+  focusedResponse: {
+    padding: 10,
     borderWidth: 1,
     borderColor: "black",
     borderRadius: 10
   },
-  unfocusedInput: {
-    padding: 5,
+  unfocusedResponse: {
+    padding: 10,
     borderWidth: 1,
     borderColor: "#CDCDCD",
     borderRadius: 10
