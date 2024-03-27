493257dcc1a98540c41c2c1f7aebf2a35ab9c06f
joey-tsai <joeytsai712@gmail.com>
2024-03-27 18:56:02 -0400

    s

--- frontend/app/Profile.tsx
+++ frontend/app/Profile.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { Text, View } from "react-native";
 import Navbar from "../components/Layout/Navbar";
-import LifestyleReligion from "./Onboarding/LifestyleReligion";
 
 export default function Profile() {
   return (
-    // <View style={{ flex: 1, justifyContent: "space-between" }}>
-    //   <Text style={{ fontFamily: "DMSansRegular" }}>Profile</Text>
-    //   <Navbar />
-    // </View>
-    <LifestyleReligion />
+    <View style={{ flex: 1, justifyContent: "space-between" }}>
+      <Text style={{ fontFamily: "DMSansRegular" }}>Profile</Text>
+      <Navbar />
+    </View>
   );
 }
