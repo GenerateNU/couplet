7b624af3bbfb4517b73f974a5a5c42d1b98ec94c
Jackson Terrill <77407057+jtrrll@users.noreply.github.com>
2024-04-06 19:08:15 -0400

    Ez connect 2 (#78)
    # Description
    Allow expo go to automatically connect to the backend

--- frontend/api/client.ts
+++ frontend/api/client.ts
@@ -1,4 +1,4 @@
 import createClient from "openapi-fetch";
 import type { paths } from "./schema.d";
 
-export default createClient<paths>({ baseUrl: `${process.env.API}:${process.env.PORT}` });
+export default createClient<paths>({ baseUrl: `${process.env.REACT_APP_API}` });
