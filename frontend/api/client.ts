cd80fa28f85c260cd7955610a9330be3cffee76c
Jackson Terrill <77407057+jtrrll@users.noreply.github.com>
2024-04-06 18:30:26 -0400

    script for getting IP when running expo apps (#76)
    # Description
    Run `API=$(./get_ip.sh) task run` to connect the frontend to the backend
    when running Expo on your phone

--- frontend/api/client.ts
+++ frontend/api/client.ts
@@ -1,4 +1,4 @@
 import createClient from "openapi-fetch";
 import type { paths } from "./schema.d";
 
-export default createClient<paths>({ baseUrl: `http://localhost:8080` }); // set to your IP when running locally
+export default createClient<paths>({ baseUrl: process.env.API });
