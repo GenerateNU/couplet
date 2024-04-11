327bc926ed2af8c48ccb6430e350491b828a0d25
Dessy Dusichka <78527291+ddusichka@users.noreply.github.com>
2024-04-11 11:47:48 -0400

    Event collection pages! (#84)
    # Description
    
    Adds a page for viewing events in a particular collection, accessible by
    clicking on the collection name from the homepage. Waiting for the
    events endpoint to properly wire it up.
    
    Please include a summary of the changes and the related issue. Please
    also
    include relevant motivation, context, and images!
    
    # How Has This Been Tested?
    
    <img
    src="https://github.com/GenerateNU/couplet/assets/78527291/fc249ca5-380d-40c7-81bd-c7a71d4cbe5b"
    alt="IMG_7249" height="600">
    
    
    Please describe the tests that you ran to verify your changes. If they
    are unit
    tests, provide the file name the tests are in. If they are not unit
    tests,
    describe how you tested the change.
    
    # Checklist
    
    - [x] I have performed a self-review of my code
    - [x] I have reached out to another developer to review my code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [x] New and existing unit tests pass locally with my changes

--- frontend/app/Event.tsx
+++ frontend/app/Event.tsx
@@ -1,7 +1,5 @@
-import { useLocalSearchParams, useRouter } from "expo-router";
+import { useLocalSearchParams } from "expo-router";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Button } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import CardStack from "../components/Event/CardStack";
 
@@ -10,31 +8,14 @@ export default function Event() {
     collectionId: string;
     eventId: string;
   }>();
-  const router = useRouter();
-
   // TODO: I think we need a notion of collectionId, which can be how we separate events into HomePageSections (rows)
   // We probably want to pass collectionId to the CardStack so it can fetch that collection's items
-  // console.log(collectionId);
 
   return (
     <SafeAreaView>
-      <View style={styles.buttonContainer}>
-        <Button onPress={() => router.back()}>
-          <Text style={{ fontFamily: "DMSansRegular" }}>Go Back</Text>
-        </Button>
-      </View>
-      <View style={styles.cardContainer}>
-        <CardStack startingEventId={eventId || ""} />
-      </View>
+      <CardStack startingEventId={eventId || ""} />
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  buttonContainer: {
-    transform: [{ translateY: -50 }]
-  },
-  cardContainer: {
-    transform: [{ translateY: -50 }]
-  }
-});
+// const styles = StyleSheet.create({});
