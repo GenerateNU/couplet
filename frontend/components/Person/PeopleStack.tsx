2ed01039660287123aba15018cfb19441c05672b
Karyna Yen <karynayen@gmail.com>
2024-04-16 22:19:13 -0400

    connect backend to populate people

--- frontend/components/Person/PeopleStack.tsx
+++ frontend/components/Person/PeopleStack.tsx
@@ -1,11 +1,14 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { View } from "react-native";
 import { getUsers } from "../../api/users";
 import Navbar from "../Layout/Navbar";
 import Person from "./Person";
-import { PersonProps } from "./PersonProps";
+import { EventCardItemProps, PersonProps } from "./PersonProps";
+import { getEvents } from "../../api/events";
 
 type User = Awaited<ReturnType<typeof getUsers>>[number];
+type Event = Awaited<ReturnType<typeof getEvents>>[number];
+
 export type PeopleStackProps = {
   userId: string;
 };
@@ -181,14 +184,70 @@ export default function PeopleStack({ userId }: PeopleStackProps) {
     [people, currentCardIndex]
   );
 
-  // TODO need use effect at all hmm
-  getUsers().then((fetchedPeople: User[]) => {
-    fetchedPeople.forEach((fetchedPerson) => {
-      // TODO: create the person object
-      const newPerson = person1;
-      setPeople((prevPeople) => [...prevPeople, newPerson]);
-    });
-  });
+
+  
+  useEffect(() => { 
+    getUsers().then((fetchedPeople: User[]) => {
+        fetchedPeople.forEach((fetchedPerson, index) => {
+            const events : EventCardItemProps[] = [];
+            getEvents({ limit: 4, offset: index }).then((fetchedEvents: Event[]) => {
+                fetchedEvents.forEach((fetchedEvent: Event) => {
+                    events.push({
+                        title: fetchedEvent.name,
+                        description: fetchedEvent.bio,
+                        imageUrl: fetchedEvent.images[0],
+                    
+                    })
+                });
+            });    
+            const newPerson : PersonProps=  {
+                id: fetchedPerson.id,
+                firstName: fetchedPerson.firstName,
+                lastName: fetchedPerson.lastName,
+                age: fetchedPerson.age,
+                pronouns: "they/them",
+                location: "San Francisco",
+                school: "UC Berkeley",
+                work: "Software Engineer",
+                height: {
+                feet: 5,
+                inches: 11
+                },
+                promptQuestion: "What is your favorite food?",
+                promptResponse:
+                "I am a person \ni am from earth. I have five feet and one eye. \nIf i had one wish in this entire world it would be to eat grapes and cheese for the rest of this short life that i have left to life. ",
+                interests: ["swimming", "running", "sleeping", "coding"],
+            
+                relationshipType: "Long Term Relationship",
+                religion: "None",
+                politicalAffiliation: "Democrat",
+                alchoholFrequency: "Never",
+                smokingFrequency: "Never",
+                drugFrequency: "Never",
+                cannabisFrequency: "Never",
+            
+                instagramUsername: "@john_doe",
+                mutualEvents: events,
+                images: [
+                {
+                    image: fetchedPerson.images[0]  },
+                {
+                    image: fetchedPerson.images[1], 
+                    caption: "This is a caption"
+                },
+                {
+                    image: fetchedPerson.images[2],
+                    caption: "This is a caption"
+                }
+                ],
+                isMatched: false,
+                likesYou: false,
+                handleReact: () => {}
+            };
+            setPeople((prevPeople) => [...prevPeople, newPerson]);
+        });
+    })
+}, [userId]);
 
   return (
     <View style={{ flex: 1, justifyContent: "space-between" }}>
